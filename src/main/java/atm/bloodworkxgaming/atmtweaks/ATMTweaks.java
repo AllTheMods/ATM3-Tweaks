ce2b1fefb1fa11993c0410e68cb13008f93bd719
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2018-08-24 15:47:51 +0200

    version bump to 1.2

--- src/main/java/atm/bloodworkxgaming/atmtweaks/ATMTweaks.java
+++ src/main/java/atm/bloodworkxgaming/atmtweaks/ATMTweaks.java
@@ -22,7 +22,7 @@ import static atm.bloodworkxgaming.atmtweaks.ATMTweaks.VERSION;
 )
 public class ATMTweaks {
     public static final String MODID = "atmtweaks";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static final CreativeTabs creativeTabATM = new ATMCreativeTab();
 
     @SidedProxy(
